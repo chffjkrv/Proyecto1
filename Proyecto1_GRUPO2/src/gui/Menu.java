232944ebec2e0d52c30754336a6ec033449eddfe
Mohibul <moihbul@moihbul.com>
2019-07-29 13:19:00 +0200

    soy mohibul y soy muy feo con gafas

---
+++ Proyecto1_GRUPO2/src/gui/Menu.java
@@ -0,0 +1,12 @@
+package gui;
+
+public class Menu {
+	
+	public static void mostrarMenu() {
+		System.out.println(" ");
+		System.out.println(" Sistema de vídeo streaming ");
+		System.out.println("****************************************");
+		System.out.println(" Por favor, escoga una opcion: ");
+		System.out.println("    1.-Añadir/crear peliculas");
+	}
+}
