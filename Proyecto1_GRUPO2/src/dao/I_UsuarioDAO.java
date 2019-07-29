26b6656d7b95d7e4e68262427c1ab218926837c6
Edmund Home <edmundhome@hotmail.com>
2019-07-29 13:54:20 +0200

    Creacion de DaeException, I_usuarioDAO, clase Usuario

--- Proyecto1_GRUPO2/src/dao/I_UsuarioDAO.java
+++ Proyecto1_GRUPO2/src/dao/I_UsuarioDAO.java
@@ -1,5 +1,17 @@
 package dao;
+import modelo.Usuario;
 
-public class I_UsuarioDAO {
-holi
+
+
+public interface I_UsuarioDAO extends AutoCloseable {
+
+    public void add(Usuario usu) throws DAOException;
+
+    public void update(Usuario usu) throws DAOException;
+
+    public void delete(String nombre) throws DAOException;
+
+    public Usuario findById(int id) throws DAOException;
+
+    public Usuario[] getAllUsuarios() throws DAOException;
 }
---
+++ Proyecto1_GRUPO2/src/modelo/Usuario.java
@@ -0,0 +1,79 @@
+package modelo;
+
+import java.sql.Date;
+
+public class Usuario {
+	private String nombre;
+	private Date fechaNacimiento;
+	private String ciudad;
+	
+	
+	public Usuario(String nombre, Date fechaNacimiento, String ciudad) {
+		super();
+		this.nombre = nombre;
+		this.fechaNacimiento = fechaNacimiento;
+		this.ciudad = ciudad;
+	}
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	public String getCiudad() {
+		return ciudad;
+	}
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((ciudad == null) ? 0 : ciudad.hashCode());
+		result = prime * result + ((fechaNacimiento == null) ? 0 : fechaNacimiento.hashCode());
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (ciudad == null) {
+			if (other.ciudad != null)
+				return false;
+		} else if (!ciudad.equals(other.ciudad))
+			return false;
+		if (fechaNacimiento == null) {
+			if (other.fechaNacimiento != null)
+				return false;
+		} else if (!fechaNacimiento.equals(other.fechaNacimiento))
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Usuario [nombre=" + nombre + ", fechaNacimiento=" + fechaNacimiento + ", ciudad=" + ciudad + "]";
+	}
+
+	
+	
+}
