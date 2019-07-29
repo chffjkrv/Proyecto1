3706e5fe4a36beb5b0594b485255113a4754f920
Edmund Home <edmundhome@hotmail.com>
2019-07-29 17:03:28 +0200

    actualizacion 17.03

--- Proyecto1_GRUPO2/src/dao/I_UsuarioDAO.java
+++ Proyecto1_GRUPO2/src/dao/I_UsuarioDAO.java
@@ -5,13 +5,13 @@ import modelo.Usuario;
 
 public interface I_UsuarioDAO extends AutoCloseable {
 
-    public void add(Usuario usu) throws DAOException;
+    public void addUsuario(Usuario usu) throws DAOException;
 
-    public void update(Usuario usu) throws DAOException;
+    public void updateUsuario(Usuario usu) throws DAOException;
 
-    public void delete(String nombre) throws DAOException;
+    public void deleteUsuario(String nombre) throws DAOException;
 
-    public Usuario findById(int id) throws DAOException;
+    public Usuario findByIdUsuario(int id) throws DAOException;
 
     public Usuario[] getAllUsuarios() throws DAOException;
 }
