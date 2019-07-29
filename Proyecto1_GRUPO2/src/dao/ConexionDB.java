a869cfa4165494bae5549aea8f92ca5ce1ccf5a4
ajj6 <adrianjimenezj@gmail.com>
2019-07-29 13:32:32 +0200

    conector bd

---
+++ Proyecto1_GRUPO2/src/dao/ConexionDB.java
@@ -0,0 +1,42 @@
+package dao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionDB {
+	Connection connection = null;
+    String BD = "proyecto1g2bd";
+    String driverClassName = "com.mysql.jdbc.Driver";
+    String driverUrl = "jdbc:mysql://localhost/" + BD;
+    String user = "root";
+    String password = "root";
+
+    public ConexionDB() {
+        try {
+          
+            Class.forName(driverClassName);
+            connection = DriverManager.getConnection(driverUrl, user, password);
+        } catch (ClassNotFoundException e) {
+            System.out.println("No se encuentra el driver");
+        } catch (SQLException e) {
+            System.out.println("Excepcion SQL: " + e.getMessage());
+            System.out.println("Estado SQL: " + e.getSQLState());
+            System.out.println("Codigo del Error: " + e.getErrorCode());
+            System.out.println("ERROR. No se puede conectar con la Bases de Datos: " + e);
+            System.exit(-1);
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void close() {
+        try {
+            connection.close();
+        } catch (SQLException se) {
+            System.out.println("Exception closing Connection: " + se);
+        }
+
+}
+}
