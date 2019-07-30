88175ef5f7e27c84c8b1786ffa69abdf1485e73f
Juan Alfonso <juanalfonso111@gmail.com>
2019-07-30 18:53:16 +0200

    UsuarioServ

--- Proyecto1_GRUPO2/src/dao/UsuarioDAOFactory.java
+++ Proyecto1_GRUPO2/src/dao/UsuarioDAOFactory.java
@@ -4,5 +4,17 @@ public class UsuarioDAOFactory {
 	public I_UsuarioDAO createUsuarioDAO() {
 		return new UsuarioDAOJDBCImpl();
 	}
+	
+	public I_UsuarioDAO updateUsuarioDAO() {
+		return new UsuarioDAOJDBCImpl();
+	}
+	
+	public I_UsuarioDAO deleteUsuarioDAO() {
+		return new UsuarioDAOJDBCImpl();
+	}
+	
+	public I_UsuarioDAO findUsuarioDAO() {
+		return new UsuarioDAOJDBCImpl();
+	}
 }
 
